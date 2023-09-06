3c384da96ce8276a464c7c2d8e133e9b6939092e
2spooky555 <rdeivert@icloud.com>
2023-09-06 00:34:25 -0700

    updated prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,7 @@
+pub use crate::app::*;
 pub use crate::components::*;
 pub use crate::resources::*;
+pub use crate::scheduler::*;
 pub use crate::systems::*;
 pub use crate::world::*;
-pub use crate::scheduler::*;
-pub use crate::app::*;
 pub use crate::*;
